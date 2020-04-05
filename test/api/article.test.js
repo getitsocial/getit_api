17e5b8b7fcde260f10173b2c081b47e13595de10
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-04-05 02:12:09 +0200

    Test: Fixed tests

--- test/api/article.test.js
+++ test/api/article.test.js
@@ -3,18 +3,21 @@ import { isJWT } from 'validator'
 import server from '~/server'
 import { serverConfig } from '~/config'
 import Model from '~/api/article/model'
+import Category from '~/api/category/model'
 import { sign } from '~/services/guard'
 import User from '~/api/user/model'
 
 
 let dataObject, 
+    defaultCategory,
     adminToken,
     defaultToken,
     apiEndpoint = 'articles'
 
 beforeEach(async (done) => {
     // Create object
-    dataObject = await Model.create({ content: 'test' })
+    defaultCategory = await Category.create({ name: 'things'} )
+    dataObject = await Model.create({ name: 'test', stock: 3, price: 4, size: 'big', currency: 'btc', category: defaultCategory._id })
     
     // Create user
     const adminUser = new User({ name: 'Maximilian', email: 'max1@moritz.com', password: 'Max123!!!', role: 'admin' })
@@ -35,56 +38,58 @@ describe(`Test /${apiEndpoint} endpoint:`, () => {
     test(`GET /${apiEndpoint} 200`, async () => {
         const {statusCode, body} = await request(server)
             .get(`${serverConfig.endpoint}/${apiEndpoint}`)
-        const firstItem = body[0]
+            .set('Authorization', 'Bearer ' + defaultToken)
         
         expect(statusCode).toBe(200)
         expect(Array.isArray(body)).toBe(true)
-        expect(typeof firstItem.content).toEqual('string')
-        expect(firstItem.content).toEqual(dataObject.content)
-        expect(firstItem.id).toBeTruthy()
-        expect(firstItem.updatedAt).toBeUndefined()
+
     })
 
     test(`GET /${apiEndpoint}:id 200`, async () => {
         const { status, body } = await request(server)
-            .get(`${serverConfig.endpoint}/${apiEndpoint}/${dataObject.id}`)
-        
+            .get(`${serverConfig.endpoint}/${apiEndpoint}/${dataObject._id}`)
+            .set('Authorization', 'Bearer ' + defaultToken)
+
         expect(status).toBe(200)
         expect(typeof body).toEqual('object')
-        expect(body.content).toEqual(dataObject.content)
     })
 
     test(`GET /${apiEndpoint}/:id 404`, async () => {
         const { status } = await request(server)
             .get(`${serverConfig.endpoint}/${apiEndpoint}/123456789098765432123456`)
+            .set('Authorization', 'Bearer ' + defaultToken)
         
         expect(status).toBe(404)
     })
-    
     test(`POST /${apiEndpoint} 201`, async () => {
+
+
         const { status, body } = await request(server)
             .post(`${serverConfig.endpoint}/${apiEndpoint}`)
-            .send({content: 'hello world'})
+            .set('Authorization', 'Bearer ' + defaultToken)
+            .send({ name: 'test', stock: 3, price: 4, size: 'big', currency: 'btc', category: defaultCategory._id })
         
         expect(status).toBe(201)
         expect(typeof body).toEqual('object')
-        expect(body.content).toEqual('hello world')
     })
-    
+
     test(`PATCH /${apiEndpoint}/:id 200`, async () => {
         const { status, body } = await request(server)
             .patch(`${serverConfig.endpoint}/${apiEndpoint}/${dataObject.id}`)
-            .send({ content: 'newContent' })
+            .set('Authorization', 'Bearer ' + defaultToken)
+            .send({ price: 4 })
         
         expect(status).toBe(200)
         expect(typeof body).toEqual('object')
-        expect(body.content).toEqual('newContent')
+        expect(body.price).toEqual(4)
     })
 
+         
     test(`PATCH /${apiEndpoint}/:id 404`, async () => {
         const { status } = await request(server)
             .patch(`${serverConfig.endpoint}/${apiEndpoint}/123456789098765432123456`)
-            .send({ content: 'test' })
+            .set('Authorization', 'Bearer ' + defaultToken)
+            .send({ description: 'test' })
         
         expect(status).toBe(404)
     })
@@ -92,20 +97,23 @@ describe(`Test /${apiEndpoint} endpoint:`, () => {
     test(`DELETE /${apiEndpoint}/:id 200`, async () => {
         const { status } = await request(server)
             .delete(`${serverConfig.endpoint}/${apiEndpoint}/${dataObject.id}`)
-        
+            .set('Authorization', 'Bearer ' + defaultToken)
+
         expect(status).toBe(200)
     })
-
+    
     test(`DELETE /${apiEndpoint}/:id 404`, async () => {
         const { status } = await request(server)
             .delete(`${serverConfig.endpoint}/${apiEndpoint}/123456789098765432123456`)
-        
+            .set('Authorization', 'Bearer ' + defaultToken)
+    
         expect(status).toBe(404)
     })
 
     test(`DELETE /${apiEndpoint}/all 401`, async () => {
         const { status, body, header } = await request(server)
             .delete(`${serverConfig.endpoint}/${apiEndpoint}/all`)
+            .set('Authorization', 'Bearer ' + defaultToken)
         
         expect(status).toBe(401)
         expect(header['content-type']).toBe('application/json')
@@ -128,5 +136,4 @@ describe(`Test /${apiEndpoint} endpoint:`, () => {
         expect(status).toBe(401)
     })
     
-
 })
