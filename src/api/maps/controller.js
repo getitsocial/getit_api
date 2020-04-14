51aa6a3a530ea5931b10ee0552518863590261ab
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-04-14 23:46:03 +0200

    Refactor: add additional language filter to map query

--- src/api/maps/controller.js
+++ src/api/maps/controller.js
@@ -4,9 +4,9 @@ import { BadRequestError } from 'restify-errors'
 const apiKey = process.env.HERE_API
 
 // https://developer.here.com/documentation/geocoder-autocomplete/dev_guide/topics/quick-start-get-suggestions.html
-const autocompleteUrl = `https://autocomplete.geocoder.ls.hereapi.com/6.2/suggest.json?apiKey=${apiKey}`
+const autocompleteUrl = `https://autocomplete.geocoder.ls.hereapi.com/6.2/suggest.json?country=DEU&language=DE&apiKey=${apiKey}`
 
-export const search = async(req, res) => {
+export const search = async(req, res, next) => {
     try {
         const { suggestions } = await request({ uri: autocompleteUrl, json: true, qs: req.query})
         res.json(suggestions)
