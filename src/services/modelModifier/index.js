75a276388353c17fe612f02e8548c9a811d9718f
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-04-21 10:23:08 +0200

    Style: clean-up

--- src/services/modelModifier/index.js
+++ src/services/modelModifier/index.js
@@ -1,25 +1,30 @@
 import { UnauthorizedError, BadRequestError } from 'restify-errors'
 
-export const addAuthor = () => 
-    (({ user, body }, res, next) => {
-        if(!user)
-            next(new UnauthorizedError())
-        if(!body)
-            next(new BadRequestError())
-        body.author = user
-        body.user = user
-        next()
-    })
+export const addAuthor = () => (({ user, body }, res, next) => {
+    
+    if(!user)
+        next(new UnauthorizedError())
+
+    if(!body)
+        next(new BadRequestError())
+    
+    body.author = user
+    body.user = user
+
+    next()
+})
     
 
-export const addShop = () => 
-    (({ user, body }, res, next) => {
-        const {shop} = user
-        console.log(shop)
-        if(!user || !user.shop)
-            next(new UnauthorizedError())
-        if(!body)
-            next(new BadRequestError())
-        body.shop = shop
-        next()
-    })
+export const addShop = () => (({ user, body }, res, next) => {
+    const { shop } = user
+
+    if(!user || !user.shop)
+        next(new UnauthorizedError())
+    
+    if(!body)
+        next(new BadRequestError())
+    
+    body.shop = shop
+
+    next()
+})
