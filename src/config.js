dbf715c91ec75cdb6b51ff9f07718cb24509f1f4
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-04-25 13:14:08 +0200

    Refactor: Moved cloudinary config to global config file

--- src/config.js
+++ src/config.js
@@ -54,3 +54,9 @@ export const i18nConfig = {
     directory: './locales',
     defaultLocale: 'en',
 }
+
+export const cloudinaryConfig = {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,    
+}
--- src/services/mediaupload/index.js
+++ src/services/mediaupload/index.js
@@ -1,11 +1,8 @@
 import cloudinary from 'cloudinary'
+import { cloudinaryConfig } from '~/config'
 
 // Cloudinary configuration
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+cloudinary.config(cloudinaryConfig)
 
 export const mediaSettings = (folder) => ({
     tags: ['bucket', 'temporary'],
