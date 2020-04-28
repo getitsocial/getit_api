0ee7b19b86874cc87b4d2a7bc1a68033a7984a5d
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-04-28 23:49:20 +0200

    Test: Added modelModifier tests

---
+++ test/api/modelModifier.test.js
@@ -0,0 +1,108 @@
+import 'dotenv/config'
+import request from 'supertest'
+import server from '~/server'
+import { sign, doorman } from '~/services/guard'
+import { isJWT } from 'validator'
+import { Router } from 'restify-router'
+import { addShop, addAuthor } from '~/services/modelModifier'
+import User from '~/api/user/model'
+import Shop from '~/api/shop/model'
+
+
+let adminUser,
+    adminToken,
+    shop,
+    apiEndpoint = 'test'
+
+beforeAll(async (done) => {
+
+    adminUser = await User.create({ name: 'Maximilian', email: 'max1@moritz.com', password: 'Max123!!!', role: 'admin' })
+
+    shop = await Shop.create({ 
+        name: 'shopname', 
+        size: 3, 
+        category: 'clothing',
+        contact: { 
+            phone: 12345
+        }, 
+        companyType: 'EU',
+        author: adminUser._id,
+        address: { 
+            label: 'Goethestraße 26, 76135 Karlsruhe, Deutschland',
+            city: 'Karlsruhe',
+            country: 'DEU', 
+            county: 'Karlsruhe (Stadt)',
+            district: 'Weststadt',
+            houseNumber: 4,
+            locationId: 'NT_0OLEZjK0pT1GkekbvJmsHC_yYD', 
+            state: 'Baden-Württemberg', 
+            street: 'Goethestrasse 26', 
+            postalCode: 76135 
+        }
+    })
+
+    adminUser.shops.push(shop._id)
+    adminUser.activeShop = shop._id
+    await adminUser.save()
+
+    // Sign in user
+    adminToken = await sign(adminUser)
+    expect(isJWT(adminToken)).toBe(true)
+
+ 
+    describe('Add Testrouter', () => {
+        const router = new Router()
+
+        router.post('/test/shop', [doorman(['admin', 'user']), addShop()], (({ body }, res, next) => {
+            try {
+                res.json({ ...body })
+            } catch (error) {      
+                next(error)
+            }
+        }))
+
+        router.post('/test/author', [doorman(['admin', 'user']), addAuthor()], (({ body }, res, next) => {
+            try {
+                res.json({ ...body })
+            } catch (error) {      
+                next(error)
+            }
+        }))
+
+
+        router.applyRoutes(server)
+    })
+
+
+    done()
+})
+describe('modelModifier Test:',  () => {
+
+    test(`GET /${apiEndpoint} 200 addShop`, async (done) => {
+        const { body, statusCode, header } = await request(server)
+            .post(`/${apiEndpoint}/shop`)
+            .set('Authorization', 'Bearer ' + adminToken)
+            .send({ hello: 'there' })
+
+        
+        expect(statusCode).toBe(200)
+        expect(header['content-type']).toBe('application/json')
+        expect(body.shop).toBe(shop._id.toString()) 
+
+        done()
+    })
+
+    test(`GET /${apiEndpoint} 200 addAuthor`, async (done) => {
+        const { body, statusCode, header } = await request(server)
+            .post(`/${apiEndpoint}/author`)
+            .set('Authorization', 'Bearer ' + adminToken)
+            .send({ hello: 'there' })
+            
+        expect(statusCode).toBe(200)
+        expect(header['content-type']).toBe('application/json')
+        expect(body.author._id).toBe(adminUser._id.toString()) 
+
+        done()
+    })
+
+})
