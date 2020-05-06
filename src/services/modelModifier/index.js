d799843d9ba6b83e54031727668a18ca2001767a
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-05-06 21:55:44 +0200

    Revert "Fix: Fix modelmodifier?"
    This reverts commit eeaeb3a5b4175b13b601a034b19b9243c20cb79c.

--- src/services/modelModifier/index.js
+++ src/services/modelModifier/index.js
@@ -1,4 +1,4 @@
-import { UnauthorizedError, BadRequestError, ResourceNotFoundError } from 'restify-errors'
+import { UnauthorizedError, BadRequestError } from 'restify-errors'
 import User from '~/api/user/model'
 import Shop from '~/api/shop/model'
 
@@ -29,29 +29,25 @@ export const addShop = () => ( async({ user, body }, res, next) => {
     }
 
     const fullUser = await User.findById(user)
+
     body.shop = fullUser.activeShop
 
     next()
 })
 
-export const showShop = () => (async(req, res, next) => {
-
+export const showShop = () => ( async(req, res, next) => {
     const { user } = req
 
-    if (!user) {
-        return next(new UnauthorizedError())
+    if(user) {
+        const fullUser = await User.findById(user)
+        console.log(fullUser)
+        try {
+            req.shop = await Shop.findById(fullUser.activeShop)
+        } catch(error) {
+            console.log(error)
+        }
     }
-
-    const fullUser = await User.findById(user._id)
     
-    const shop = await Shop.findById(fullUser.activeShop)
-    
-    if (!shop) {
-        return next(new ResourceNotFoundError())
-    }
-
-    req.shop = shop
-
     next()
 
 })
--- test/api/services/modelModifier.test.js
+++ test/api/services/modelModifier.test.js
@@ -12,73 +12,13 @@ import { defaultShopData } from '../data'
 
 let adminUser,
     adminToken,
-    defaultToken,
     shop,
     apiEndpoint = 'test'
 
 beforeAll(async (done) => {
 
-    const router = new Router()
-
-    router.get('/test/showShop', [doorman(['admin', 'user']), showShop()], ((req, res, next) => {
-        try {
-            next(res.json({ shop }))
-        } catch (error) {      
-            next(error)
-        }
-    }))
-        
-    router.get('/test/showShop/nodoorman', [showShop()], ((req, res, next) => {
-        try {
-            next(res.json({ shop }))
-        } catch (error) {      
-            next(error)
-        }
-    }))
-        
-
-    router.post('/test/shop', [doorman(['admin', 'user']), addShop()], (({ body }, res, next) => {
-        try {
-            next(res.json({ ...body }))
-        } catch (error) {      
-            next(error)
-        }
-    }))
-
-    router.post('/test/shop/nodoorman', addShop(), (({ body }, res, next) => {
-        try {
-            next(res.json({ ...body }))
-        } catch (error) {      
-            next(error)
-        }
-    }))
-
-
-    router.post('/test/author', [doorman(['admin', 'user']), addAuthor()], (({ body }, res, next) => {
-        try {
-            next(res.json({ ...body }))
-        } catch (error) {      
-            next(error)
-        }
-    }))
-
-    router.post('/test/author/nodoorman', addAuthor(), (({ body }, res, next) => {
-        try {
-            next(res.json({ ...body }))
-        } catch (error) {      
-            next(error)
-        }
-    }))
-
-    router.applyRoutes(server)
-    done()
-})
-
-beforeEach(async (done) => {
-
-    const defaultUser = await User.create({ name: 'Maximilian', email: 'max42@moritz.com', password: 'Max123!!!', role: 'admin' })
-
     adminUser = await User.create({ name: 'Maximilian', email: 'max1@moritz.com', password: 'Max123!!!', role: 'admin' })
+
     shop = await Shop.create(defaultShopData({ author: adminUser._id }))
 
     adminUser.shops.push(shop._id)
@@ -88,43 +28,70 @@ beforeEach(async (done) => {
     // Sign in user
     adminToken = await sign(adminUser)
     expect(isJWT(adminToken)).toBe(true)
+
+ 
+    describe('Add Testrouter', () => {
+        const router = new Router()
+
+
+        router.get('/test/showShop', [doorman(['admin', 'user']), showShop()], (({ shop }, res, next) => {
+            try {
+                res.json({ shop })
+            } catch (error) {      
+                next(error)
+            }
+        }))
+
+
+        router.post('/test/shop', [doorman(['admin', 'user']), addShop()], (({ body }, res, next) => {
+            try {
+                res.json({ ...body })
+            } catch (error) {      
+                next(error)
+            }
+        }))
+
+        router.post('/test/shop/nodoorman', addShop(), (({ body }, res, next) => {
+            try {
+                res.json({ ...body })
+            } catch (error) {      
+                next(error)
+            }
+        }))
+
+
+        router.post('/test/author', [doorman(['admin', 'user']), addAuthor()], (({ body }, res, next) => {
+            try {
+                res.json({ ...body })
+            } catch (error) {      
+                next(error)
+            }
+        }))
+
+        router.post('/test/author/nodoorman', addAuthor(), (({ body }, res, next) => {
+            try {
+                res.json({ ...body })
+            } catch (error) {      
+                next(error)
+            }
+        }))
+
+        router.applyRoutes(server)
+    })
+
+
     done()
-
-    // Sign in user
-    defaultToken = await sign(defaultUser)
-    expect(isJWT(defaultToken)).toBe(true)
-    done()    
-    
 })
-
 describe('modelModifier Test:',  () => {
 
     test(`GET /${apiEndpoint} 200 showShop`, async (done) => {
         const { body, statusCode, header } = await request(server)
             .get(`/${apiEndpoint}/showShop`)
             .set('Authorization', 'Bearer ' + adminToken)
-
         expect(statusCode).toBe(200)
         expect(header['content-type']).toBe('application/json')
         expect(body.shop._id).toBe(adminUser.activeShop.toString()) 
 
-        done()
-    })
-
-    test(`GET /${apiEndpoint} 401 showShop missing token`, async (done) => {
-        const { statusCode } = await request(server)
-            .get(`/${apiEndpoint}/showShop`)
-
-        expect(statusCode).toBe(401)
-        done()
-    })
-
-    test(`GET /${apiEndpoint} 401 showShop no doorman`, async (done) => {
-        const { statusCode } = await request(server)
-            .get(`/${apiEndpoint}/showShop/nodoorman`)
-            .set('Authorization', 'Bearer ' + adminToken)
-            
-        expect(statusCode).toBe(401)
         done()
     })
 
@@ -195,5 +162,5 @@ describe('modelModifier Test:',  () => {
 
         done()
     })
- 
+
 })
