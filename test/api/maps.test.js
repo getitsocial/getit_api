11192755adcb9da3b086103abd1669d6bae37c70
Jonas Scholz <jonas.scholz@bbscholz.de>
2020-05-23 13:04:33 +0200

    Test: Added maps/detail test

--- test/api/maps.test.js
+++ test/api/maps.test.js
@@ -12,12 +12,17 @@ let defaultUser,
 beforeEach(async (done) => {
 
     // Create user
-    defaultUser =  await User.create({ name: 'Maximilian', email: 'max2@moritz.com', password: 'Max123!!!', role: 'user' })
+    defaultUser =  await User.create({
+        name: 'Maximilian',
+        email: 'max2@moritz.com',
+        password: 'Max123!!!',
+        role: 'user'
+    })
 
-    // Sign in 
+    // Sign in
     defaultToken = await sign(defaultUser)
     expect(isJWT(defaultToken)).toBe(true)
-    
+
     done()
 })
 
@@ -30,7 +35,26 @@ describe(`Test /${apiEndpoint} endpoint:`, () => {
 
         expect(statusCode).toBe(200)
         expect(typeof body).toEqual('object')
-        
+
+    })
+
+    test(`GET /${apiEndpoint}/detail 200`, async () => {
+        const { statusCode, body } = await request(server)
+            .get(`${serverConfig.endpoint}/${apiEndpoint}/detail?locationid=NT_0OLEZjK0pT1GkekbvJmsHC_yYD`)
+            .set('Authorization', 'Bearer ' + defaultToken)
+
+        expect(statusCode).toBe(200)
+        expect(typeof body).toEqual('object')
+
+    })
+
+    test(`GET /${apiEndpoint}/detail 400`, async () => {
+        const { statusCode } = await request(server)
+            .get(`${serverConfig.endpoint}/${apiEndpoint}/detail?cyka`)
+            .set('Authorization', 'Bearer ' + defaultToken)
+
+        expect(statusCode).toBe(400)
+
     })
 
 })
